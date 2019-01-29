eef475394ca6751c4b2116d10b003b26fad3bf01
Agata <kozinska.agata@gmail.com>
2019-01-29 01:21:55 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import "./index.scss";
+
+import "slick-carousel/slick/slick.scss";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+
+AOS.init({
+  offset: 10,
+  duration: 800,
+  easing: 'ease-in-sine',
+  delay: 500,
+});
