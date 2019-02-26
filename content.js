37dcdc28bf6d08f1bc49a409a921d0280ea28f2a
Agata <kozinska.agata@gmail.com>
2019-02-26 22:46:38 +0100

    Change poi to webpack

--- content.js
+++
@@ -1,5 +0,0 @@
-let variables = {
-
-}
-
-module.exports = variables;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import "./index.scss";
 
-import "slick-carousel/slick/slick.scss";
+require('normalize.css/normalize.css');
+import './index.scss';
+
 import AOS from "aos";
 import "aos/dist/aos.css";
-
 
 AOS.init({
   offset: 10,
   duration: 800,
   easing: 'ease-in-sine',
   delay: 500,
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
 });
