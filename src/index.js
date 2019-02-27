ba00192a413cdfbc5ee30f5aa87575dfb7a33523
Agata <kozinska.agata@gmail.com>
2019-02-27 14:09:38 +0100

    Fix nav and add menu for mobile

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 require('normalize.css/normalize.css');
 import './index.scss';
 
@@ -13,6 +12,44 @@ AOS.init({
 });
 
 document.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector('.header__menu');
 
+  window.addEventListener('scroll', () => {
+    const menuHeight = menu.offsetHeight;
+    const pageHeight = window.scrollY;
+
+    if (pageHeight > menuHeight) {
+      menu.classList.add('scroll');
+    } else {
+      menu.classList.remove('scroll');
+    }
+  });
+
+
+  const button = document.querySelector('.hamburger__button');
+  const navVisible = 'nav--visible';
+
+  button.addEventListener('click', () => {
+    menu.classList.toggle(navVisible);
+    stopScroll();
+  });
+
+  function stopScroll() {
+    if (menu.classList.contains(navVisible)) {
+      document.documentElement.classList.add("no-scroll");
+    } else {
+      document.documentElement.classList.remove("no-scroll");
+    }
+  }
+
+  const links = document.querySelectorAll('.nav__list__item');
+  const linksArray = [...links];
+
+  for (let i = 0; i < linksArray.length; i++) {
+    linksArray[i].addEventListener("click", function () {
+      document.documentElement.classList.remove("no-scroll");
+      menu.classList.remove(navVisible);
+    });
+  }
 
 });
